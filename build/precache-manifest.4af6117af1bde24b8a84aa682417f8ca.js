7755d96bcce0aadbfa15657686e8f01ffbf44422
Nick <neverate@gmail.com>
2019-07-30 18:17:41 +0300

    uibuild

---
+++ build/precache-manifest.4af6117af1bde24b8a84aa682417f8ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd1c3be23aa2fe3a2cf7c90bb3b46af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e4a3930ed8345517294",
+    "url": "/static/css/main.3c16ff8a.chunk.css"
+  },
+  {
+    "revision": "da776ff6faf2792a84a5",
+    "url": "/static/js/2.47e200b3.chunk.js"
+  },
+  {
+    "revision": "6e4a3930ed8345517294",
+    "url": "/static/js/main.9921d7f0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
