6427bb7b87b2e5ae7724646d7dc995cb99342f52
Nick <neverate@gmail.com>
2019-07-31 19:02:02 +0300

    finished exercises up to 3.18

--- index.js
+++ index.js
@@ -29,6 +29,91 @@ app.use(
   })
 );
 
+app.post("/api/people", (req, res) => {
+  const body = req.body;
+
+  if (!body || !body.name || !body.number) {
+    return res.status(400).send({
+      error: `name or number missing`
+    });
+  }
+
+  const entry = new Entry({
+    name: body.name,
+    number: body.number
+  });
+
+  entry
+    .save()
+    .then(savedEntry => {
+      res.json(savedEntry.toJSON());
+    })
+    .catch(error => next(error));
+});
+
+app.put("/api/people/:id", (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+
+  if (!body || !body.name || !body.number) {
+    return res.status(400).send({
+      error: `name or number missing`
+    });
+  }
+
+  const updatedEntry = {
+    name: body.name,
+    number: body.number
+  };
+
+  Entry.findByIdAndUpdate(id, updatedEntry, { new: true })
+    .then(savedEntry => {
+      res.json(savedEntry.toJSON());
+    })
+    .catch(error => next(error));
+});
+
+app.get("/api/info", (req, res) => {
+  Entry.find({})
+    .then(entries => {
+      console.log(entries);
+      const resStr = `Phonebook has info on ${
+        entries.length
+      } people<br/>${Date()}`;
+      res.send(resStr);
+    })
+    .catch(error => next(error));
+});
+
+app.get("/api/people", (req, res) => {
+  Entry.find({})
+    .then(entries => {
+      console.log(entries);
+      res.json(entries.map(entry => entry.toJSON()));
+    })
+    .catch(error => next(error));
+});
+
+app.get("/api/people/:id", (req, res) => {
+  Entry.findById(req.params.id)
+    .then(result => {
+      console.log(result);
+      res.json(result.toJSON());
+    })
+    .catch(error => next(error));
+});
+
+app.delete("/api/people/:id", (req, res, next) => {
+  Entry.findByIdAndRemove(req.params.id)
+    .then(result => {
+      console.log(result);
+      res.status(204).end();
+    })
+    .catch(error => next(error));
+});
+
+//old version of api :
+
 let entries = [
   {
     name: "Arto Hellas",
@@ -57,6 +142,16 @@ let entries = [
   }
 ];
 
+const generateNewId = () => {
+  let currentIds = entries.map(entry => entry.id);
+  do {
+    var id = Math.floor(Math.random() * 1000) + 1;
+    console.log(id);
+  } while (currentIds.indexOf(id) !== -1);
+
+  return id;
+};
+
 app.post("/api/persons", (req, res) => {
   let body = req.body;
   if (!body || !body.name || !body.number) {
@@ -79,26 +174,8 @@ app.post("/api/persons", (req, res) => {
   res.send(`new entry was added to the phonebook`);
 });
 
-const generateNewId = () => {
-  let currentIds = entries.map(entry => entry.id);
-  do {
-    var id = Math.floor(Math.random() * 1000) + 1;
-    console.log(id);
-  } while (currentIds.indexOf(id) !== -1);
-
-  return id;
-};
-
 app.get("/api/persons", (req, res) => {
   res.send(entries);
-});
-
-app.get("/api/people", (request, response) => {
-  Entry.find({}).then(entries => {
-    console.log(entries);
-    response.json(entries.map(entry => entry.toJSON()));
-    //response.json(notes);
-  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
@@ -133,6 +210,18 @@ const unknownEndpoint = (req, res) => {
 };
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError" && error.kind == "ObjectId") {
+    return res.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
